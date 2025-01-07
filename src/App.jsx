f0cc4c0b87539f29bfa52aafbe306415a9e85162
OGlamp <lamparellin@gmail.com>
2025-01-07 17:35:44 -0500

    4.5 test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import CitySearch from "./components/CitySearch";
-import EventList from "./components/EventList";
-import NumberOfEvents from "./components/NumberOfEvents";
-import { useEffect, useState } from "react";
-import { extractLocations, getEvents } from "./api";
-
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import CitySearch from "./CitySearch";
+import EventList from "./EventList";
 
 const App = () => {
+  const [searchParams] = useSearchParams();
+  const [token, setToken] = useState(null);
   const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
-  const [allLocations, setAllLocations] = useState([]);
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    const accessToken = searchParams.get("access_token");
+    if (accessToken) {
+      setToken(accessToken);
+      fetchEvents(accessToken);
+    }
+  }, [searchParams]);
 
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    setEvents(allEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
+  const fetchEvents = async (accessToken) => {
+    const response = await fetch(
+      `https://afbpzo8aj0.execute-api.us-east-1.amazonaws.com/dev/api/get-events/${accessToken}`
+    );
+    const data = await response.json();
+    setEvents(data.events);
   };
 
   return (
-    <div className="App">
-      <CitySearch allLocations={allLocations} />
-      <NumberOfEvents />
-      <EventList events={events} />
+    <div>
+      <h1>Event Search</h1>
+      {token ? (
+        <>
+          <CitySearch />
+          <EventList events={events} />
+        </>
+      ) : (
+        <p>Please authorize to search for events.</p>
+      )}
     </div>
   );
 };
