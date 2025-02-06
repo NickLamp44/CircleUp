06a8a5c4301ba52d3db218ef37910d966af98742
OGlamp <lamparellin@gmail.com>
2025-02-05 22:03:04 -0500

    weird react strict side effect?

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,8 @@ import "./main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 serviceWorkerRegistration.register();
